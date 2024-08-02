ddccb549f0c59ea62c303af56c13606cec633858
maxzoladz <maximilian.zoladz@5minds.de>
2024-08-02 09:18:17 +0200

    cash task

---
+++ 01/cash/cash.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(void){
+
+    int change;
+    int coinCounter = 0;
+
+    printf("Change Owed: ");
+    scanf("%i", &change);
+
+    if(change >= 25){
+        while(change >= 25){
+            change -= 25;
+            coinCounter++;
+        }
+    }
+
+    if(change >= 10){
+        while(change >= 10){
+            change -= 10;
+            coinCounter++;
+        }
+    }
+
+    if(change >= 5){
+        while(change >= 5){
+            change -= 5;
+            coinCounter++;
+        }
+    }
+
+    if(change >= 1){
+        while(change >= 1){
+            change -= 1;
+            coinCounter++;
+        }
+    }
+
+    printf("%i", coinCounter);
+    printf("\n");
+}
