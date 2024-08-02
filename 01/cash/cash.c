abd4c2684ad3f0a40c4e2a7b56f3ec7d98fca95c
maxzoladz <maximilian.zoladz@5minds.de>
2024-08-02 14:03:28 +0200

    removed if-statement above while loop

--- 01/cash/cash.c
+++ 01/cash/cash.c
@@ -8,35 +8,27 @@ int main(void) {
     printf("Change Owed: ");
     scanf("%i", &change);
 
-    if(change >= 25){
         while(change >= 25){
             change -= 25;
             coinCounter++;
         }
-    }
 
-    if(change >= 10){
         while(change >= 10){
             change -= 10;
             coinCounter++;
         }
-    }
 
-    if(change >= 5){
         while(change >= 5){
             change -= 5;
             coinCounter++;
         }
-    }
 
-    if(change >= 1){
         while(change >= 1){
             change -= 1;
             coinCounter++;
         }
-    }
 
     printf("%i", coinCounter);
     printf("\n");
-    
+
 }
